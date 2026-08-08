9d9dab588b2ac64d594201503c73052c1f76c805
saqimtiaz <saq.imtiaz@gmail.com>
2026-08-08 11:02:31 +0200

    fix: ESM module fixes

--- bin/secure-contact.js
+++ bin/secure-contact.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
-const path = require("path");
+import path from "node:path";
+import {fileURLToPath} from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 if(process.argv.includes("--assets")){
   process.stdout.write(path.join(__dirname, "..", "src") + "\n");
